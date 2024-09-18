d15e9514be441488c71afd5b2d2b7665c68dbcb9
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2024-09-18 09:47:23 +0000

    map exemples

--- map.js
+++ map.js
@@ -5,16 +5,27 @@
  */
 
 // Using a for loop
-
+let nums = [1,2,3,4,5];
+let results = [];
+for (let num of nums) {
+  results.push (num * 2);
+}
+console.log(results);
 
 // Using map()
 
-
 // Simplified w/ map()
 
+//const multByTwo = num => num*2;
+//const mapResults = nums.map(multByTwo); //Callback the multByTwo function
+//console.log(mapResults);
 
 // Simplfied w/ map() + arrow function
 
+
+const multByTwo = num => num*2;
+const mapResults = nums.map(num => num*2); //Callback the multByTwo function
+console.log(mapResults);
 
 // With objects:
 const students = [
@@ -37,3 +48,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentWithId = students.map(student => student.name + " : " + student.id);
+console.log (studentWithId );
